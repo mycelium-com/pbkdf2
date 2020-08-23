b36215a9c04058d375c478aab400fa79737328ef
CryptoManiac <balthazar@yandex.ru>
2020-08-23 20:55:02 +0300

    cdecl

--- include/pbkdf.h
+++ include/pbkdf.h
@@ -4,6 +4,10 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * PBKDF2_SHA256(passwd, passwdlen, salt, saltlen, c, buf, dkLen):
  * Compute PBKDF2(passwd, salt, c, dkLen) using HMAC-SHA256 as the PRF, and
@@ -17,4 +21,10 @@ void pbkdf2_sha256(const uint8_t * passwd, size_t passwdlen, const uint8_t * sal
  * write the output to buf.  The value dkLen must be at most 32 * (2^32 - 1).
  */
 void pbkdf2_sha3_256(const uint8_t * passwd, size_t passwdlen, const uint8_t * salt, size_t saltlen, uint64_t c, uint8_t * buf, size_t dkLen);
+
+#ifdef __cplusplus
+}
+#endif
+
+
 #endif
